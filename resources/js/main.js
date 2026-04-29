1b8334250a59b7618e4f27a1dc41bd2ffdd36e10
jsegra9 <jsegra9@gmail.com>
2026-04-29 17:38:01 +0200

    Me section

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,23 @@
-function scrollToSection(id) {
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+    OnScroll()
+};
+
+// Get the header
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function OnScroll() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function ScrollToSection(id) {
     document.getElementById(id).scrollIntoView({behavior: 'smooth'});
 }
